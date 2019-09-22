8f90965b719e0fa3c4d5fea8abc2df1fcc58c7a5
has3ong <khsh5592@naver.com>
2019-09-22 20:32:38 +0900

    2019-09-22

--- 1_Spark-Basic/word-count.py
+++ 1_Spark-Basic/word-count.py
@@ -1,4 +1,10 @@
 from pyspark import SparkConf, SparkContext
+import os
+
+os.environ["PYSPARK_PYTHON"]="/usr/local/Cellar/python/3.7.4/bin/python3"
+os.environ["PYSPARK_DRIVER_PYTHON"]="/usr/local/Cellar/python/3.7.4/bin/python3"
+
+__BASE_DIR__ = os.path.dirname(os.path.realpath(__file__))
 
 conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf = conf)
